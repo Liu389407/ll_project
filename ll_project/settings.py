7278b5c6d1bb2499bc1c1e6b33cf3dbd734c995c
liujie <liujie@example.com>
2025-11-13 00:46:22 +0800

    修复第141行语法错误：删除多余的中文字符

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -144,7 +144,7 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
-        }
+        },,,
     }'''
 
 # Password validation
